a274229993f65170e3c58adcc0979c212baf9cf6
Fred <felick4000@gmail.com>
2022-09-05 15:25:10 -0400

    good enough

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+    it('renders', () => {
+        render(<Nav />);
+      });
+
+      it('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
+        // assert value comparison
+      });
+
+})
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+    // Arrange
+    const { getByLabelText } = render(<Nav />);
+
+    // Assert
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+    })
+  })
+
+  describe('links are visible', () => {
+    it('inserts text into the links', () => {
+      const { getByTestId } = render(<Nav />);
+      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+      expect(getByTestId('about')).toHaveTextContent('About me');
+    });
+  })
+
+
+
